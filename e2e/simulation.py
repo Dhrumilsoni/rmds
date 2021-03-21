3828ee0d14c82879b442ff472629d4b429934312
Dhrumilsoni <dhrumilv@usc.edu>
2021-03-20 22:56:47 -0700

    Implemented multiple new simulators
    S2 considers lower bound and actual prediction and performs safely but gets less stock returns than S1.
    
    S3 considers lower bound and current value. One edge case here is that if lower bound is greater than the current value then invest all.

--- e2e/simulation.py
+++ e2e/simulation.py
@@ -102,7 +102,6 @@ class S1Simulator(AbstractSimulator):
         self.stock_val[model.company]["upper_bound"].append(confidence_interval["95"][1].values[0])
 
     def complete(self):
-        print(self.stock_val)
         current_money = self.initial_amount
         current_stocks = 0
         all_val = self.stock_val[self.stock[0]]
@@ -118,3 +117,110 @@ class S1Simulator(AbstractSimulator):
 
             net_worth.append(current_money+current_stocks*all_val["cur_val"][ind])
         pd.Series(net_worth).plot()
+
+
+@SimulatorFactory.register('S2')
+class S2Simulator(AbstractSimulator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # if there are more than one stocks. S2 shouldn't run.
+        # TODO: Think about multiple stocks anytime soon.
+        assert len(self.stock) == 1, "S2 should have only one stock."
+        self.prediction_window = 1
+
+    # TODO: add "add" function in the parent class instead of the child class.
+    def add(self, model: Model, df_test_ground_truth: Dict[str, pd.Series], df_train: Dict[str, pd.DataFrame]) -> None:
+        df_test_for_prediction = {}
+        for col in df_test_ground_truth:
+            if col != constants.STOCK_COLUMN:
+                df_test_for_prediction[col] = df_test_ground_truth[col]
+
+        prediction, confidence_interval = model.predict(self.prediction_window, df_test_for_prediction)
+        self.stock_val[model.company] = self.stock_val.get(model.company, {
+                                                                    "date": [],
+                                                                    "cur_val": [],
+                                                                    "next_val": [],
+                                                                    "lower_bound": [],
+                                                                    "upper_bound": []
+                                                                })
+
+        date = datetime.datetime.strptime(get_date_minus_days(prediction.index[0].strftime(constants.DATE_FORMAT), 1), constants.DATE_FORMAT)
+        self.stock_val[model.company]["date"].append(date)
+        self.stock_val[model.company]["cur_val"].append(df_train[constants.STOCK_COLUMN][constants.STOCK_COLUMN][date])
+        self.stock_val[model.company]["next_val"].append(prediction.values[0])
+        self.stock_val[model.company]["lower_bound"].append(confidence_interval["95"][0].values[0])
+        self.stock_val[model.company]["upper_bound"].append(confidence_interval["95"][1].values[0])
+
+    def complete(self):
+        current_money = self.initial_amount
+        current_stocks = 0
+        all_val = self.stock_val[self.stock[0]]
+        net_worth = []
+        for ind, date in enumerate(all_val["date"]):
+            if all_val["cur_val"][ind] < all_val["next_val"][ind]:
+                money_to_invest = current_money*(1-(all_val["next_val"][ind]-all_val["lower_bound"][ind])/(all_val["upper_bound"][ind]-all_val["lower_bound"][ind]))
+                current_stocks += money_to_invest / all_val["cur_val"][ind]
+                current_money -= money_to_invest
+
+            if all_val["cur_val"][ind] > all_val["next_val"][ind]:
+                current_money += current_stocks*all_val["cur_val"][ind]
+                current_stocks = 0
+
+            net_worth.append(current_money+current_stocks*all_val["cur_val"][ind])
+        pd.Series(net_worth).plot()
+
+
+@SimulatorFactory.register('S3')
+class S3Simulator(AbstractSimulator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # if there are more than one stocks. S2 shouldn't run.
+        # TODO: Think about multiple stocks anytime soon.
+        assert len(self.stock) == 1, "S3 should have only one stock."
+        self.prediction_window = 1
+
+    # TODO: add "add" function in the parent class instead of the child class.
+    def add(self, model: Model, df_test_ground_truth: Dict[str, pd.Series], df_train: Dict[str, pd.DataFrame]) -> None:
+        df_test_for_prediction = {}
+        for col in df_test_ground_truth:
+            if col != constants.STOCK_COLUMN:
+                df_test_for_prediction[col] = df_test_ground_truth[col]
+
+        prediction, confidence_interval = model.predict(self.prediction_window, df_test_for_prediction)
+        self.stock_val[model.company] = self.stock_val.get(model.company, {
+                                                                    "date": [],
+                                                                    "cur_val": [],
+                                                                    "next_val": [],
+                                                                    "lower_bound": [],
+                                                                    "upper_bound": []
+                                                                })
+
+        date = datetime.datetime.strptime(get_date_minus_days(prediction.index[0].strftime(constants.DATE_FORMAT), 1), constants.DATE_FORMAT)
+        self.stock_val[model.company]["date"].append(date)
+        self.stock_val[model.company]["cur_val"].append(df_train[constants.STOCK_COLUMN][constants.STOCK_COLUMN][date])
+        self.stock_val[model.company]["next_val"].append(prediction.values[0])
+        self.stock_val[model.company]["lower_bound"].append(confidence_interval["95"][0].values[0])
+        self.stock_val[model.company]["upper_bound"].append(confidence_interval["95"][1].values[0])
+
+    def complete(self):
+        current_money = self.initial_amount
+        current_stocks = 0
+        all_val = self.stock_val[self.stock[0]]
+        net_worth = []
+        for ind, date in enumerate(all_val["date"]):
+            if all_val["cur_val"][ind] < all_val["next_val"][ind]:
+                if all_val["cur_val"][ind]<all_val["lower_bound"][ind]:
+                    # invest all
+                    money_to_invest = current_money
+                else:
+                    money_to_invest = current_money*(1-(all_val["cur_val"][ind]-all_val["lower_bound"][ind])/(all_val["upper_bound"][ind]-all_val["lower_bound"][ind]))
+                current_stocks += money_to_invest / all_val["cur_val"][ind]
+                current_money -= money_to_invest
+
+            if all_val["cur_val"][ind] > all_val["next_val"][ind]:
+                current_money += current_stocks*all_val["cur_val"][ind]
+                current_stocks = 0
+
+            net_worth.append(current_money+current_stocks*all_val["cur_val"][ind])
+        pd.Series(net_worth).plot()
+
